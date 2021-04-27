9eb7660280327f238e0bedd6469a3b4a007efc63
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2021-04-27 15:13:42 +0530

    Configured db

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,12 +87,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
+
+#C
+
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "ENGINE" : "django.db.backends.postgresql",
+        "NAME": 'd7p0l3i19v1mp5',
+        "HOST": 'ec2-54-163-254-204.compute-1.amazonaws.com',
+        "PORT": 5432,
+        "USER": 'lzmiuclzycjpha',
+        "PASSWORD": env('PASSWORD'),
+
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
