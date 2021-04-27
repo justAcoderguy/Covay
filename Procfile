4f0088174e077b56d226508acc282bff958816c8
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2021-04-27 14:57:15 +0530

    Added allowed hosts

--- Procfile
+++ Procfile
@@ -1,4 +1,1 @@
-web: python manage.py collectstatic --noinput
-web: python manage.py makemigration
-web: python manage.py migrate
 web: gunicorn mysite.wsgi 
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('just_co_away.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
